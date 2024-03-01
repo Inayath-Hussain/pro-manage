5392bb07e4ac8aeef97c16df0ff0934b73260189
Inayath <inayathh2@gmail.com>
2024-03-01 13:16:21 +0530

    FIx: UpdateTask router

--- apps/server/src/routes/task.ts
+++ apps/server/src/routes/task.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/common/auth";
 import { tryCatchWrapper } from "../utilities/requestHandlers/tryCatchWrapper";
+
 import { validateAddTaskBody } from "../middlewares/tasks/addTask";
 import { addTaskController } from "../controllers/tasks/addTask";
+
 import { getTaskController } from "../controllers/tasks/getTask";
 import { validateGetTaskQuery } from "../middlewares/tasks/getTask";
+
 import { deleteTaskController } from "../controllers/tasks/deleteTask";
 import { validateDeleteTaskParam } from "../middlewares/tasks/deleteTask";
-import { validateUpdateBody } from "../middlewares/users/update";
+
 import { updateTaskController } from "../controllers/tasks/updateTask";
+import { validateUpdateTaskBody } from "../middlewares/tasks/updateTask";
 
 const router = Router();
 
@@ -20,8 +24,8 @@ router.post("/", tryCatchWrapper(authMiddleware), validateAddTaskBody)      //mi
 router.post("/", tryCatchWrapper(addTaskController))     // controller
 
 
-router.put("/:id", tryCatchWrapper(authMiddleware), validateUpdateBody)     // middlewares
-router.put("/:id", updateTaskController)    // controller
+router.put("/", tryCatchWrapper(authMiddleware), validateUpdateTaskBody)     // middlewares
+router.put("/", updateTaskController)    // controller
 
 
 router.delete("/:id", tryCatchWrapper(authMiddleware), validateDeleteTaskParam)      // middlewares
