659c2514b812df595b158ccf93300b771300055b
Inayath <inayathh2@gmail.com>
2024-03-05 23:45:57 +0530

    Fix: analytics api
    added a class to initialize data with zero and then update them using service.

--- apps/server/src/controllers/tasks/analytics.ts
+++ apps/server/src/controllers/tasks/analytics.ts
@@ -6,6 +6,7 @@ import { expireAccessTokenCookie } from "../../utilities/cookies/signAccessToken
 import { expireRefreshTokenCookie } from "../../utilities/cookies/signRefreshToken";
 import { Ierror } from "../../utilities/requestHandlers/errorHandler";
 import { taskService } from "../../services/task";
+import { TaskAnalytics } from "@pro-manage/common-interfaces";
 
 export const getAnalyticsController: RequestHandler = async (req, res, next) => {
     const email = req.email as string
@@ -21,18 +22,8 @@ export const getAnalyticsController: RequestHandler = async (req, res, next) => 
 
     const result = await taskService.getAnalytics(userDoc._id)
 
-    console.log(result[0])
-
-    const analytics = {
-        backlog: result[0].backlog[0].count,
-        progress: result[0].progress[0].count,
-        todo: result[0].todo[0].count,
-        done: result[0].done[0].count,
-        high: result[0].high[0].count,
-        moderate: result[0].moderate[0].count,
-        low: result[0].low[0].count,
-        dueDate: result[0].dueDate[0].count
-    }
+    const analytics = new TaskAnalytics();
+    analytics.addAnalytics(result)
 
     return res.status(200).json({ message: "success", analytics })
 }
--- libs/common-interfaces/src/lib/tasks/analytics.ts
+++ libs/common-interfaces/src/lib/tasks/analytics.ts
@@ -8,3 +8,36 @@ export interface IAnalytics {
     moderate: number
     dueDate: number
 }
+
+
+export class TaskAnalytics implements IAnalytics {
+    backlog: number;
+    done: number;
+    dueDate: number;
+    high: number;
+    low: number;
+    moderate: number;
+    progress: number;
+    todo: number;
+
+    constructor() {
+        this.backlog = 0;
+        this.progress = 0;
+        this.todo = 0
+        this.done = 0;
+
+        this.high = 0;
+        this.moderate = 0;
+        this.low = 0;
+        this.dueDate = 0;
+    }
+
+    addAnalytics(analytics: any) {
+        type IKeys = keyof IAnalytics
+        const keys = Object.keys(analytics[0]) as IKeys[]
+
+        keys.forEach(k => {
+            if (analytics[0][k].length > 0) this[k] = analytics[0][k][0].count
+        })
+    }
+}
