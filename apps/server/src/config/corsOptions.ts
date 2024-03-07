676e78fae68b4bd32618b319a0d071f10222f47c
Inayath <inayathh2@gmail.com>
2024-03-07 19:42:19 +0530

    Add: cors options for production

--- apps/server/src/config/corsOptions.ts
+++ apps/server/src/config/corsOptions.ts
@@ -4,7 +4,7 @@ import { env } from "./env"
 export const corsOptions: CorsOptions = env.isProd ?
     {
         // add react deployed origin here.
-        origin: [],
+        origin: ["https://pro-manage-lime.vercel.app/"],
         credentials: true
     }
     :
